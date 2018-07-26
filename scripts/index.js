e6cee2f120144b08b5b94863524e7237f6ed6261
Alex Puhl <apuhlswimmer@hotmail.com>
2018-07-26 12:43:36 -0600

    got the createBookmarks setup and successfully tested it

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,14 @@
 'use strict';
-/* global $ */
+/* global $, api */
 
 
 $(document).ready(function() {
 
+});
+
+
+api.createBookmark('google', 'https://google.com', 'website for finding stuff on the internet', '5', (newItem) => {
+  api.getBookmarks((items) => {
+    console.log(items);
+  });
 });
